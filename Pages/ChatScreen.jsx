d0f8b6d23b48fe856b6b9e97da9d1ea881be623e
Ke Dat <datmartin10@gmail.com>
2024-05-13 16:51:03 +0700

    first commit

---
+++ Pages/ChatScreen.jsx
@@ -0,0 +1,168 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import {
+  Bubble,
+  GiftedChat,
+  InputToolbar,
+  Send,
+} from "react-native-gifted-chat";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import ChatFaceData from "../Services/ChatFaceData";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+CHAT_BOT_FACE =
+  "https://res.cloudinary.com/dknvsbuyy/image/upload/v1685678135/chat_1_c7eda483e3.png";
+baseUrl = "https://f01e-2402-800-61f8-ea23-28c0-825-546d-7c52.ngrok-free.app";
+export default function ChatScreen() {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [chatFaceColor, setChatFaceColor] = useState();
+  const [botId, setBotId] = useState(ChatFaceData[0].id);
+
+  useEffect(() => {
+    checkFaceId();
+  }, []);
+
+  const checkFaceId = async () => {
+    const id = await AsyncStorage.getItem("chatFaceId");
+    CHAT_BOT_FACE = id ? ChatFaceData[id]?.image : ChatFaceData[0].image;
+    setChatFaceColor(ChatFaceData[id]?.primary);
+    setBotId(ChatFaceData[id]?.bot_id);
+    setMessages([
+      {
+        _id: 1,
+        text: "Xin chào, tôi có thể giúp gì cho bạn?",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: CHAT_BOT_FACE,
+        },
+      },
+    ]);
+  };
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    if (messages[0].text) {
+      getResponse(messages[0].text);
+    }
+  }, []);
+
+  const getResponse = async (msg) => {
+    setLoading(true);
+    try {
+      setLoading(true);
+      await fetch(`${baseUrl}/chat/chat_with_documents`, {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          bot_id: "ktcn",
+          question: msg,
+        }),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const chatAIResp = {
+            _id: Math.random() * (9999999 - 1),
+            text: data.answer.output_text,
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "React Native",
+              avatar: CHAT_BOT_FACE,
+            },
+          };
+          setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, chatAIResp)
+          );
+          setLoading(false);
+        });
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#671ddf",
+          },
+          left: {},
+        }}
+        textStyle={{
+          right: {
+            // fontSize:20,
+            padding: 2,
+          },
+          left: {
+            color: "#671ddf",
+            // fontSize:20,
+            padding: 2,
+          },
+        }}
+      />
+    );
+  };
+
+  const renderInputToolbar = (props) => {
+    //Add the extra styles via containerStyle
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={{
+          padding: 3,
+
+          backgroundColor: "#671ddf",
+          color: "#fff",
+        }}
+        textInputStyle={{ color: "#fff" }}
+      />
+    );
+  };
+
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View style={{ marginRight: 10, marginBottom: 5 }}>
+          <FontAwesome
+            name="send"
+            size={24}
+            color="white"
+            resizeMode={"center"}
+          />
+        </View>
+      </Send>
+    );
+  };
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <GiftedChat
+        messages={messages}
+        isTyping={loading}
+        multiline={true}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+        renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
+        renderSend={renderSend}
+      />
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Demonstrates integration with expo-yarn-workspaces, to have apps inside the apps directory and shared libs in the packages directory.
+
+Mostly just following the expo-yarn-workspaces README after creating a minimal package.json in the root, as described by the Yarn workspaces documentation.
+
+Follow these below steps to start:
+1. Open terminal and run 'yarn'
+2. After done step 1:
+- Chung, QAnh: run 'yarn start', it will generate a QR code, open Expo app in mobile and scan the QR 
+- An: Can follow Chung, QAnh if you want to test on mobile, beside that, you can dev on laptop by running Android studio
+  + Start a virtual machine on Android studio and run 'yarn start --android'
